66b6e3134a4107c443b2b5624116f859890fa0ca
Cesar Landeros <chichozell@gmail.com>
2016-09-12 01:56:34 -0500

    added create card token method for ios

--- RNOpenpay/RNOpenpay.m
+++ RNOpenpay/RNOpenpay.m
@@ -9,23 +9,47 @@
 #import "Openpay.h"
 #import "RNOpenpay.h"
 
+@interface RNOpenpayManager()
+
+Openpay *openpayAPI;
+
+@end
+
 @implementation RNOpenpayManager
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(getDeviceSessionId:(NSString *)merchantId
-                          withApiKey:(NSString *)apiKey
-                       forProduction:(BOOL)isProduction
-                            resolver:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(setup:(NSString *)merchantId
+             withApiKey:(NSString *)apiKey
+          forProduction:(BOOL)isProduction)
+{
+    _openpayAPI = [[Openpay alloc] initWithMerchantId:merchantId
+                                               apyKey:apiKey
+                                     isProductionMode:isProduction];
+}
+
+RCT_EXPORT_METHOD(createCardToken:(NSDictionary *)cardJson)
+{
+    OPCard *card = [[OPCard alloc] init];
+    card.holderName = [cardJson[@"holder_name"]];
+    card.number = [cardJson[@"holder_name"]];
+    card.expirationMonth = [cardJson[@"holder_name"]];
+    card.expirationYear = [cardJson[@"holder_name"]];
+    card.cvv2 = [cardJson[@"holder_name"]];
+
+
+    [_openpayAPI createTokenWithCard:card
+        success:^(OPToken *token) {
+            resolve(token.id);
+        } failure:^(NSError *error) {
+            reject(@"token error", @"could not generate token", error);
+    }];
+}
+
+RCT_EXPORT_METHOD(getDeviceSessionId:(RCTPromiseResolveBlock)resolve
                             rejecter:(RCTPromiseRejectBlock)reject)
 {
-    Openpay *openpayAPI = [[Openpay alloc] initWithMerchantId:merchantId
-                                                       apyKey:apiKey
-                                             isProductionMode:isProduction];
-
-    NSString *sessionId = [openpayAPI createDeviceSessionId];
-
-    resolve(sessionId);
+    resolve([_openpayAPI createDeviceSessionId]);
 }
 
 - (dispatch_queue_t)methodQueue
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { NativeModules } from 'react-native';
 
 const RNOpenpay = {
     setup(merchantId, apiKey, production = false) {
-        NativeModules.RNOpenpayManager.setup(merchantId, apiKey, production = false)
+        NativeModules.RNOpenpayManager.setup(merchantId, apiKey, production)
     },
     createCardToken: NativeModules.RNOpenpayManager.createCardToken,
     getDeviceSessionId: NativeModules.RNOpenpayManager.getDeviceSessionId,
